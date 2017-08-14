7bc8a80b472282719dd5bf65c2ef70d6eb710b33
Manohar-Reddy <30863354+Manohar-Reddy@users.noreply.github.com>
2017-08-14 12:31:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one', function (req,res) {
     res.send('Article one will be requested and will be served here');
 });
 
-app.get('article-two', function (req,res) {
+app.get('/article-two', function (req,res) {
     res.send('Article two will be requested and will be served here');
 });
 
-app.get('article-three', function (req,res) {
+app.get('/article-three', function (req,res) {
     res.send('Article three will be requested and will be served here');
 });
 
