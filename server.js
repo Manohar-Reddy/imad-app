b032286574381daae1566837ddb0cd7c61c4148a
Manohar-Reddy <30863354+Manohar-Reddy@users.noreply.github.com>
2017-08-15 18:50:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,13 +102,6 @@ app.get('/:articleName', function (req,res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
-});
-
-app.get('/article-three', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
