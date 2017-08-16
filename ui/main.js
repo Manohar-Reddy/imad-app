26da709f6dd123764a686018de63479f9b346138
Manohar-Reddy <30863354+Manohar-Reddy@users.noreply.github.com>
2017-08-16 05:30:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 // chamge the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+// Move the new image
+var img = document.getElementById('img');
+img.onclick = function() {
+  img.style.marginleft = '100px';  
+};
