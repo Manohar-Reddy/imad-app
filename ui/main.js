a4cd615685ba36aac6f7a6b0c999b57f0e072640
Manohar-Reddy <30863354+Manohar-Reddy@users.noreply.github.com>
2017-08-26 11:17:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,12 @@ element.innerHTML = 'New Value';
 
 // Move the  image
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight () {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + px;
+}
 img.onclick = function() {
     img.style.marginLeft = '100px';
     var interval = setInterval(moveRight,50);  
 };
-var marginleft = 0;
-function moveRight () {
-    marginleft = marginleft + 1;
-    img.style.marginleft = marginleft + 3;
-}
